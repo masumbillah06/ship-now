8ea708900f6167fc477b9ac1b871d7ca2e2d32a8
Masum Billah <mbmasum06@gmail.com>
2026-08-03 01:13:31 +0600

    Dashboard layout size propotins fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,34 +13,42 @@ import { stats } from "@/lib/data";
 export default function Home() {
   return (
     <DashboardShell>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-        {/* Main column (2/3) */}
-        <div className="space-y-4 lg:col-span-2">
-          {/* Stat cards row - confined to main column only */}
+      <div className="flex flex-col gap-4 lg:flex-row">
+        {/* Main column - wider than sidebar */}
+        <div className="flex flex-col gap-4 lg:w-[76%]">
+          {/* Stat cards row */}
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
             {stats.map((s) => (
               <StatCard key={s.id} {...s} />
             ))}
           </div>
 
-          {/* Shipment Statistic + Profit Summary */}
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-            <ShipmentStatisticChart />
-            <ProfitSummaryChart />
+          {/* Shipment Statistic (narrower) + Profit Summary (wider) */}
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <div className="sm:w-[42%]">
+              <ShipmentStatisticChart />
+            </div>
+            <div className="sm:w-[58%]">
+              <ProfitSummaryChart />
+            </div>
           </div>
 
-          {/* Product Categories + Tracking */}
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-            <ProductCategories />
-            <TrackingCard />
+          {/* Product Categories (narrower) + Tracking (wider) */}
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <div className="sm:w-[42%]">
+              <ProductCategories />
+            </div>
+            <div className="sm:w-[58%]">
+              <TrackingCard />
+            </div>
           </div>
 
           {/* Recent Shipments - full width of main column */}
           <RecentShipments />
         </div>
 
-        {/* Sidebar column (1/3) - spans full height of main column */}
-        <div className="space-y-4">
+        {/* Sidebar column - narrower, full height */}
+        <div className="flex flex-col gap-4 lg:w-[24%]">
           <ShipmentTypeChart />
           <ShipmentAlerts />
           <RecentActivity />
